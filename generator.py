f5215f0479aa5028ddb8f323a16689918581d2a9
Jesse <jetskee96@yahoo.com>
2023-05-30 11:59:18 -0700

    Update generator.py
    Improved script generation and added a feature to update existing script

--- generator.py
+++ generator.py
@@ -14,24 +14,61 @@ def generate_code(task_description):
             {"role": "system", "content": "You are a helpful AI code assistant."},
             {"role": "user", "content": prompt},
         ],
-        max_tokens=300,
+        max_tokens=500,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    code = response.choices[0].message['content'].strip()
-    return code
+    
+    message = response['choices'][0]['message']
+    if message['role'] == 'assistant':
+        content = message['content']
+        # Filter out everything before the first block of Python code
+        match = re.search(r'```python([\s\S]*?)```', content)
+        if match:
+            code = match.group(1)
+        else:
+            code = ""
+    else:
+        code = ""
+    return code.strip()
 
 def main():
-    task_description = input("Enter the description of the app you want to create: ")
-    app_name = input("Enter the name for the new app (without .py): ")
+    action = input("Do you want to create a new script or update an existing one? Enter 'new' or 'update': ")
+    task_description = input("Enter the description of the task you want to accomplish: ")
 
     code = generate_code(task_description)
-    
-    with open(f"{app_name}.py", "w") as app_file:
-        app_file.write(code)
 
-    print(f"App created: {app_name}.py")
+    if action.lower() == 'new':
+        script_name = input("Enter the name for the new script (without .py): ")
+        script_path = f"{script_name}.py"
+        mode = 'w'
+        message = f"Script created: {script_path}"
+    else:  # 'update'
+        script_path = input("Enter the path of the script you want to update (ensure it's a .py file): ")
+        if not script_path.endswith('.py'):
+            script_path += '.py'
+        mode = 'a'
+        message = f"Script updated: {script_path}"
+    
+    # For debugging: print the content of the existing file before appending
+    if mode == 'a':
+        with open(script_path, 'r') as script_file:
+            print("\nContent of the existing file before appending:\n")
+            print(script_file.read())
+
+    with open(script_path, mode) as script_file:
+        if mode == 'a':
+            script_file.write("\n\n# Generated code\n")
+        script_file.write(code)
+
+    print(message)
+
+    # For debugging: print the content of the file after appending
+    if mode == 'a':
+        with open(script_path, 'r') as script_file:
+            print("\nContent of the file after appending:\n")
+            print(script_file.read())
 
 if __name__ == "__main__":
     main()
